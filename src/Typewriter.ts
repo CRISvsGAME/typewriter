52d1324173ddf743d0376235dd1b165e113e88ab
CRISvsGAME <cris@crisvsgame.com>
2026-07-03 20:00:00 +0000

    Typewriter 0.0.8
    Add Constants

--- src/Typewriter.ts
+++ src/Typewriter.ts
@@ -1,1 +1,8 @@
+const ROOT_CLASS = "cvg-typewriter";
+const TEXT_CLASS = "cvg-typewriter__text";
+const VIEW_CLASS = "cvg-typewriter__view";
+const ROOT_SELECTOR = `.${ROOT_CLASS}`;
+const TEXT_SELECTOR = `.${TEXT_CLASS}`;
+const VIEW_SELECTOR = `.${VIEW_CLASS}`;
+
 export class Typewriter {}
