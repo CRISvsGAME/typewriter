4c68047266cd607365daaab456dd8f5170f7a8c8
CRISvsGAME <cris@crisvsgame.com>
2026-07-04 20:00:00 +0000

    Typewriter 0.0.9
    Add Typewriter Options

--- src/Typewriter.ts
+++ src/Typewriter.ts
@@ -1,3 +1,15 @@
+export type TypewriterOptions = {
+    selector: string;
+    texts: string[];
+    startDelay: number;
+    holdAfterWrite: number;
+    holdAfterDelete: number;
+    minWriteDelay: number;
+    maxWriteDelay: number;
+    minDeleteDelay: number;
+    maxDeleteDelay: number;
+};
+
 const ROOT_CLASS = "cvg-typewriter";
 const TEXT_CLASS = "cvg-typewriter__text";
 const VIEW_CLASS = "cvg-typewriter__view";
@@ -5,4 +17,26 @@ const ROOT_SELECTOR = `.${ROOT_CLASS}`;
 const TEXT_SELECTOR = `.${TEXT_CLASS}`;
 const VIEW_SELECTOR = `.${VIEW_CLASS}`;
 
-export class Typewriter {}
+const defaultTypewriterOptions: TypewriterOptions = {
+    selector: "",
+    texts: ["Typewriter", "Animation", "Library"],
+    startDelay: 100,
+    holdAfterWrite: 1000,
+    holdAfterDelete: 100,
+    minWriteDelay: 100,
+    maxWriteDelay: 200,
+    minDeleteDelay: 50,
+    maxDeleteDelay: 100,
+};
+
+export class Typewriter {
+    private typewriters: TypewriterOptions[];
+
+    constructor(typewriters: Partial<TypewriterOptions>[] = []) {
+        let t = typewriters.map((typewriter) => {
+            return { ...defaultTypewriterOptions, ...typewriter };
+        });
+
+        this.typewriters = t;
+    }
+}
